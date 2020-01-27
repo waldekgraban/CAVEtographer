c0c81effbe7d500a029b75ed76893d361f01f33f
Waldemar Graban <waldekgraban@gmail.com>
2020-01-27 13:49:49 +0100

    Get measuments points from svx file

--- src/index.php
+++ src/index.php
@@ -4,21 +4,42 @@ namespace Waldekgraban\Converter;
 
 require_once "../vendor/autoload.php";
 
+use PHPSurvex\PHPSurvex\Parser\Parser;
 use Waldekgraban\Converter\Points\Point;
 use Waldekgraban\Converter\Points\PointsCollection;
 
-$points = new PointsCollection([
-    0 => new Point(6.5, 293, -36),
-    1 => new Point(5.4, 234, -41),
-    2 => new Point(2.5, 237, -38),
-    3 => new Point(7.9, 246, -33),
-]);
+// $points = new PointsCollection([
+//     0 => new Point(6.5, 293, -36),
+//     1 => new Point(5.4, 234, -41),
+//     2 => new Point(2.5, 237, -38),
+//     3 => new Point(7.9, 246, -33),
+// ]);
 
-header("Content-type: text/csv");
-header("Content-Disposition: attachment; filename=pomiary.csv");
-header("Pragma: no-cache");
-header("Expires: 0");
+// header("Content-type: text/csv");
+// header("Content-Disposition: attachment; filename=pomiary.csv");
+// header("Pragma: no-cache");
+// header("Expires: 0");
 
-echo $points->toCsv();
+// echo $points->toCsv();
 
-echo $points->showResult();
+// echo $points->showResult();
+
+$filename = __DIR__ . '/Examples/black_hawk_down.svx';
+$content  = file_get_contents($filename);
+$parser   = Parser::make($content);
+
+$surveys = $parser->parse();
+
+$survey = $surveys->first();
+$data   = $survey->getData()->first();
+
+foreach ($data->getMeasurements() as $measurement) {
+    $points[$measurement->getValue('from')] = new Point(
+        $measurement->getValue('compass'),
+        $measurement->getValue('tape'),
+        $measurement->getValue('clino'),
+    );
+}
+$points = new PointsCollection($points);
+
+$points->showResult();
