0bdf15b907215aa37d88b9980fc71b9c18305e50
waldekgraban <waldekgraban@gmail.com>
2020-07-28 14:04:30 +0200

    update packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+//Register external packages
+    import Fragment from 'vue-fragment'
+    Vue.use(Fragment.Plugin)
+
+//Register custom components
+    // Vue.component('example-component', require('./components/Path/to/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
