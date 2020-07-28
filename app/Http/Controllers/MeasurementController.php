2692541a3c507863de9b96fe8d978ca8f6aabbf9
waldekgraban <waldekgraban@gmail.com>
2020-07-28 14:10:22 +0200

    create Measurement model and controller

---
+++ app/Http/Controllers/MeasurementController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Measurement;
+use Illuminate\Http\Request;
+
+class MeasurementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Measurement  $measurement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Measurement  $measurement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Measurement  $measurement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Measurement  $measurement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Measurement $measurement)
+    {
+        //
+    }
+}
